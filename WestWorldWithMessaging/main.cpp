3841ab9e4875dd4c82f4d94acf2ed24f6488b9d6
ps16atanaka <kyelese0617@gmail.com>
2019-06-21 22:24:25 +0900

    WestWorldWomanをコピー

--- WestWorldWithMessaging/main.cpp
+++ WestWorldWithMessaging/main.cpp
@@ -1,15 +1,27 @@
 //
 //  main.cpp
-//  WestWorldWithMessaging
+//  WestWorld1
 //
-//  Created by 田中皓己 on 2019/06/21.
+//  Created by 田中皓己 on 2019/05/23.
 //  Copyright © 2019 田中皓己. All rights reserved.
 //
 
 #include <iostream>
+#include "EntityNames.hpp"
+#include "Miner.hpp"
+#include "MinersWife.hpp"
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Miner Bob(ent_Miner_Bob);
+    MinersWife Elsa(ent_Elsa);
+    
+    
+    for(int i = 0;i < 30; ++i){
+        Bob.Update();
+        Elsa.Update();
+    }
+    
+    
     return 0;
 }
