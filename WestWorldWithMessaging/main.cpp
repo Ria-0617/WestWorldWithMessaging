193eabb058b8014ebc7069ee73e5c211ea0695af
ps16atanaka <kyelese0617@gmail.com>
2019-06-21 21:59:31 +0900

    initial commit

---
+++ WestWorldWithMessaging/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WestWorldWithMessaging
+//
+//  Created by 田中皓己 on 2019/06/21.
+//  Copyright © 2019 田中皓己. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
